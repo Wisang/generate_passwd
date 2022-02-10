535f99a3cfeda3f4c652eaa5a01463dc9f3db3ac
wisang <wseom7@gmail.com>
2022-02-10 18:03:57 +0900

    data save complete

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+def save_data():
+    f = open('data.txt', "a")
+    f.writelines(get_entry_value() + "\n")
+
+
+def get_entry_value():
+    user_data = f"{web_addr_entry.get()} | {mail_entry.get()} \
+    | {pass_entry.get()}"
+    return user_data
 # ---------------------------- UI SETUP ------------------------------- #
-from turtle import width
 
 window = Tk()
 window.config(bg="white", padx=50, pady=50)
@@ -29,13 +38,17 @@ generate_button = Button(text="Generate Password", bg="white",
                          fg="black")
 generate_button.grid(column=2, row=3)
 
+test_label = Label(text="start", bg="white", fg="black")
+test_label.grid(row=5, column=1)
+
+
 add_button = Button(text="Add", width=36, bg="white",
-                         fg="black")
+                         fg="black", command=save_data)
 add_button.grid(row=4, column=1, columnspan=2)
 
-web_addr = Entry(width=39)
-web_addr.grid(row=1, column=1, columnspan=2)
-web_addr.focus()
+web_addr_entry = Entry(width=39)
+web_addr_entry.grid(row=1, column=1, columnspan=2)
+web_addr_entry.focus()
 
 mail_entry = Entry(width=39)
 mail_entry.grid(row=2, column=1, columnspan=2)
@@ -43,9 +56,6 @@ mail_entry.insert(0, "wseom7@gmail.com")
 
 pass_entry = Entry(width=22)
 pass_entry.grid(row=3, column=1)
-
-
-
 
 
 
