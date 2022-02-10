0e217e549855f2c5cd4af8bfa2867d9a63486bfe
wisang <wseom7@gmail.com>
2022-02-10 20:40:53 +0900

    password generator completed

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
 
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    pass_entry.insert(0, password)
+    pyperclip.copy(password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def save_data():
-    f = open('data.txt', "a")
-    f.writelines(get_entry_value() + "\n")
+    website = web_addr_entry.get()
+    email = mail_entry.get()
+    password = pass_entry.get()
 
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="empty")
+    else:
+        is_ok = messagebox.askokcancel(title="website", message='ok to save?')
 
-def get_entry_value():
-    user_data = f"{web_addr_entry.get()} | {mail_entry.get()} \
-    | {pass_entry.get()}"
-    return user_data
+        if is_ok:
+            with open('data.txt', "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                web_addr_entry.delete(0, END)
+                pass_entry.delete(0, END)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -35,12 +67,8 @@ pass_label = Label(text="Password", bg="white", fg="black")
 pass_label.grid(row=3, column=0)
 
 generate_button = Button(text="Generate Password", bg="white",
-                         fg="black")
+                         fg="black", command=generate_password)
 generate_button.grid(column=2, row=3)
-
-test_label = Label(text="start", bg="white", fg="black")
-test_label.grid(row=5, column=1)
-
 
 add_button = Button(text="Add", width=36, bg="white",
                          fg="black", command=save_data)
